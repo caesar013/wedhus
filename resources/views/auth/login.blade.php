bbc76e493a050d89dd68c19dff60037ddf3c16a8
m. rom doni <m.romdoni101102@gmail.com>
2024-06-30 20:51:19 +0700

    [feat] login-blade

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>Login &mdash; Stisla</title>
+
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/components.css')}}">
+</head>
+
+<body>
+    <div id="app">
+        <section class="section">
+            <div class="d-flex flex-wrap align-items-stretch">
+                <div class="col-lg-4 col-md-6 col-12 order-lg-1 min-vh-100 order-2 bg-white">
+                    <div class="p-4 m-3">
+                        <img src="{{ asset('assets/img/logo-goat.png') }}" alt="logo" width="80"
+                            class="shadow-light rounded-circle mb-5 mt-2">
+                        <h4 class="text-dark font-weight-normal">Welcome to <span class="font-weight-bold">Wedhus</span>
+                        </h4>
+                        <p class="text-muted">Before you get started, you must login.</p>
+                        <form method="POST" action="#" class="needs-validation" novalidate="">
+                            @csrf
+                            <div class="form-group">
+                                <label for="username">Username</label>
+                                <input id="username" type="username" class="form-control" name="username" tabindex="1"
+                                    required autofocus>
+                                <div class="invalid-feedback">
+                                    Please fill in your Username
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="d-block">
+                                    <label for="password" class="control-label">Password</label>
+                                </div>
+                                <input id="password" type="password" class="form-control" name="password"
+                                    tabindex="2" required>
+                                <div class="invalid-feedback">
+                                    please fill in your password
+                                </div>
+                            </div>
+
+                            <div class="form-group text-right">
+                                <a href="auth-forgot-password.html" class="float-left mt-3">
+                                    Forget Password?
+                                </a>
+                                <button type="submit" class="btn btn-primary btn-lg btn-icon icon-right"
+                                    tabindex="4">
+                                    Login
+                                </button>
+                            </div>
+                        </form>
+
+                        <div class="text-center mt-5 text-small">
+                            Copyright &copy; <a href="https://github.com/mromdoni101102">inod</a> and <a href="https://github.com/caesar013">caesar</a>. Made with 💙 by Stisla
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-8 col-12 order-lg-2 order-1 min-vh-100 background-walk-y position-relative overlay-gradient-bottom"
+                    data-background="../assets/img/unsplash/login.jpg">
+                    <div class="absolute-bottom-left index-2">
+                        <div class="text-light p-5 pb-2">
+                            <div class="mb-5 pb-3">
+                                <h1 id="date" class="mb-2 display-4 font-weight-bold"></h1>
+                                <h5 class="font-weight-normal text-muted-transparent">Domba, Indonesia</h5>
+                            </div>
+                            Photo by <a class="text-light bb" target="_blank"
+                                href="https://unsplash.com/photos/M7E6gxb1SV4">Sheep Farm Indonesia</a> on <a
+                                class="text-light bb" target="_blank" href="https://unsplash.com">Unsplash</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+    {{-- script for date --}}
+    <script>
+        function displayGreeting() {
+            const dateElement = document.getElementById("date");
+            const hours = new Date().getHours();
+
+            if (hours >= 5 && hours < 12) {
+                dateElement.textContent = "Good Morning";
+            } else if (hours >= 12 && hours < 18) {
+                dateElement.textContent = "Good Morning";
+            } else {
+                dateElement.textContent = "Good Evening";
+            }
+        }
+
+        displayGreeting();
+    </script>
+
+    <!-- General JS Scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+    <script src="{{asset('assets/js/stisla.js')}}"></script>
+
+    <!-- Template JS File -->
+    <script src="{{asset('assets/js/scripts.js')}}"></script>
+    <script src="{{asset('assets/js/custom.js')}}"></script>
+</body>
+
+</html>
