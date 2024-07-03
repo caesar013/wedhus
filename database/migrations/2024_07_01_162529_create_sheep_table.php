7486e5130d015b5b762be0757ab76be5daf00144
cαεsαɾ <91421259+caesar013@users.noreply.github.com>
2024-07-03 10:11:21 +0700

    [FEAT] migrations

---
+++ database/migrations/2024_07_01_162529_create_sheep_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sheep', function (Blueprint $table) {
+            $table->id();
+            $table->string('code', 50);
+            $table->boolean('sex'); // 1: jantan, 0: betina
+            $table->enum('status', ['sehat', 'mati', 'sakit', 'bunting'])->default('sehat');
+            $table->decimal('weight', 5, 2)->default(0);
+            $table->date('date_of_entry');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sheep');
+    }
+};
